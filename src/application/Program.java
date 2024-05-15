7699d067895e7858771165973bb8bf44cdac6c0d
Ronaldo Vilela <ronalldo.vilela@gmail.com>
2024-05-15 09:28:30 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -23,11 +24,16 @@ public class Program {
 		
 		sellers.forEach(x -> System.out.println(x));
 		
-		System.out.println("\n=== TEST 2: seller findByIDepartment ===");
+		System.out.println("\n=== TEST 3: seller findByIDepartment ===");
 		sellers = sellerDao.findAll();
 		
 		sellers.forEach(x -> System.out.println(x));
-
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
 	}
 
 }
